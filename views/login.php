b4e1ff2e7606ec25bb8177b60c9801f30e436036
mPrg0d <ibgod@web.de>
2026-08-28 10:40:14 +0200

    fix login input field length

--- views/login.php
+++ views/login.php
@@ -38,7 +38,13 @@ body { font-family: Arial; background: #f4f4f4; }
     max-width: 400px; margin: 80px auto; background: white;
     padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);
 }
-input { padding: 10px; width: 100%; ; }
+input {  padding: 10px;
+    width: 100%;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    box-sizing: border-box;
+}
 button { padding: 10px; width: 100%; background: #444; color: white; border: none; }
 .error { color: red; }
 </style>
